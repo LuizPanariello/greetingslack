57abc9905baf9fad89f896d704ae94032ad5fee9
Luiz Panariello <luizpanariello@gmail.com>
2017-06-01 16:28:38 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,7 +25,7 @@ def parse_join(message):
         x = x.json()
         x = x["channel"]["id"]
         #xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel="+x+"&text="+urllib.quote(MESSAGE)+"&parse=full&as_user=true")
-        xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel=C235YLN49&text="+urllib.quote(MESSAGE)+"&parse=full&as_user=true")
+        xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel=G5MD8B20&text="+urllib.quote(MESSAGE)+"&parse=full&as_user=true")
 
         #DEBUG
         #print '\033[91m' + "HELLO SENT" + m["user"]["id"] + '\033[0m'
